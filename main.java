d5f561be65198f760b8744aa27debc285fa1707b
SulthanTaufiiqul <sulthantaufiqulhakim@gmail.com>
2024-02-29 09:09:51 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+public class main {
+    public static void main(String[] args) {
+        barang brg = new barang();
+        karyawan k = new karyawan();
+        laporan l = new laporan();
+        member m = new member();
+        transaksi t = new transaksi();
+    }
+}
